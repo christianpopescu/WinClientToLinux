42ed50544035b0d74ceadd6502884c4afb032bc7
Christian Popescu <christian.popescu@outlook.com>
2020-09-18 23:17:52 +0200

    add server fectory

---
+++ src/WinClientToLinuxConsole/WinClientToLinuxConsole/Server/ServerFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinClientToLinuxConsole.Server
+{
+    public class ServerFactory
+    {
+        protected Dictionary<string, LinuxServer> AvailableServers = new Dictionary<string, LinuxServer>();
+
+        public LinuxServer GetServer(string serverId)
+        {
+            return AvailableServers[serverId];
+        }
+    }
+}
